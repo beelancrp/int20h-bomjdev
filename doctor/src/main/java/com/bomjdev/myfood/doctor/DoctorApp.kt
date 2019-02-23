d94293b6bd13c9a6edd362b4a7be2d6f23e91b38
azazellj <azazellj@gmail.com>
2019-02-23 19:32:03 +0200

    additional changes

--- doctor/src/main/java/com/bomjdev/myfood/doctor/DoctorApp.kt
+++ doctor/src/main/java/com/bomjdev/myfood/doctor/DoctorApp.kt
@@ -1,5 +1,15 @@
 package com.bomjdev.myfood.doctor
 
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
 /**
  * Created by azazellj on 2/23/19.
  */
+class DoctorApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        FirebaseApp.initializeApp(this)
+    }
+}
